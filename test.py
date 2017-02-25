6f99f79d94e0212ad9f9be8b404c15a16920447d
andrew <aberger91@protonmail.com>
2017-02-25 15:42:51 -0600

    subclassed Batch with Pair class

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ and does linear regression/correlation and histogram plots
 where xs_str and ys_str are exchange product codes or yahoo stock ticker
 exchange product codes are identical to product codes on CME, ICE, LIFFE, etc ..
 '''
-from tsc import TSCPairs
+from tsc import TSCPairs, TSCBatch
         
 if __name__ == '__main__':
     from sys import argv
@@ -15,10 +15,13 @@ if __name__ == '__main__':
         ys_str = argv[2]
         start_year = argv[3]
 
-        conn = TSCPairs(xs_str, ys_str, start_year)
-        
+        conn = TSCPairs([xs_str, ys_str], start_year)
+
         conn.correlate()
         conn.plot_volatility()
+
+        bat = TSCBatch(['WEC', 'XLE', 'XOM', 'CVX', 'NG', 'CL'], 2010)
+        print(bat.head()) 
 
     else:
         raise ValueError('usage: ts_connect.py <xs_str> <ys_str> <start_year>')
--- tsc.py
+++ tsc.py
@@ -2,29 +2,54 @@
 '''
 python module for finding connections between time series of financial instruments
 '''
+__author__ = 'Andrew Berger'
 
 from seaborn import jointplot, plt
 from pandas import DataFrame
 import datetime as dt
 from quandl import get as quandl_get
+from quandl import ApiConfig
 from qproducts import QPRODUCT
 
 try:    from pandas_datareader import data as pdr
 except: raise ImportError('try: pip install pandas_datareader')
+
 
 COLUMN_NAMES = ('Adj Close', 'Settle', 'Value', 'Last')
 
 class TSCError(Exception):
     pass
     
+try:    ApiConfig.api_key = open('../quandl_api.key', 'r').read().strip()
+except: raise TSCError('could not load quandl_api_key')
 
-class RemoteDataHandler(object):
+
+class TSCFrame(object):
     '''
-    handle posts to quandl, yahoo
+    act like a df if we can else err
     '''
     def __init__(self, product, year):
         self.product = product
         self.year = year
+        self.df = DataFrame()
+
+    def __getattr__(self, value):
+        try:
+            return getattr(self.df, value)
+        except Exception as e:
+            raise TSCError('attribute %s does not exist\n%s' % (
+                            value, e))
+
+
+class TSCRemote(TSCFrame):
+    '''
+    handle posts to quandl, yahoo
+    '''
+    def __init__(self, product, year):
+        super().__init__(product, year)
+        self.df =               self._fetch_data()
+        self._col = self.name = self._get_column_name()
+        self._series =          self.df[self._col]
 
     def _quandl(self):
         _start = '%s-01-01' % self.year
@@ -53,77 +78,34 @@ class RemoteDataHandler(object):
     def _qproduct_exists(self):
         return self.product in QPRODUCT
         
-
-class TSCConnection(RemoteDataHandler):
-    '''
-    proxy for url request handler
-    '''
-    def __init__(self, product, year):
-        '''
-        fetch remote data: if not quandl, try yahoo, if not either, raise err
-        return df
-        '''
-        super().__init__(product, year)
-        self._df = self._fetch_data()
-        self._col = self._extract_col_name(self._df.columns)        
-        self._series = self._df[self._col]
-
-    def _extract_col_name(self, cols):
+    def _get_column_name(self):
         '''
         raise error unless column from the df matches parameters
         return str
         '''
         for col in COLUMN_NAMES:
-            if col in cols:
+            if col in self.df.columns:
                 return col
         raise TSCError('column does not exist in config')
-        
-    def __getattr__(self, value):
-        '''
-        act like a df if we can
-        '''
-        if hasattr(self._df, value):
-            return getattr(self._df, value)
-        else:
-            return getattr(self, value)
 
 
-class TSCBatch(object):
+class TSCBatch(TSCFrame):
     def __init__(self, tickers, year):
         '''
         lookup and fetch list of product codes
         '''
+        super().__init__(tickers, year)
         self.tickers = tickers
-        self.start_year = year
-        self.dfs = (tickers, year)
-
-    @property
-    def dfs(self):
-        return self._dfs
-
-    @dfs.setter
-    def dfs(self, values):
-        tickers, year = values
-        self._dfs = DataFrame()
         for ticker in tickers:
-            ts_object = TSCConnection(ticker, year)
-            self._dfs[ticker] = ts_object._series
-
-    def __getattr__(self, value):
-        '''
-        act like a df if we can
-        '''
-        if hasattr(self._dfs, value):
-            return getattr(self._dfs, value)
-        else:
-            return getattr(self, value)
+            ts_object = TSCRemote(ticker, year)
+            self.df[ticker] = ts_object._series
 
     def plot(self):
-        self.dfs.plot()
+        self.df.plot()
         plt.show()
 
 
-class TSCPairs(object):
+class TSCPairs(TSCBatch):
     '''
     comparisons between prices and returns for two time series
     '''
@@ -132,40 +114,23 @@ class TSCPairs(object):
         pull data for tickers and wrap into new df 
         return df
         '''
-        xs, ys = tickers
-        self._xs_str =      xs  # product code or stock symbol
-        self._ys_str =      ys  # product code or stock symbol
-        self._start_year =  year
-        self.dfs =          (xs, ys)  # delegate to setter by default
+        super().__init__(tickers, year)
+        self._xs =      self.df[tickers[0]]
+        self._ys =      self.df[tickers[1]]
         self._init()
 
-    @property
-    def dfs(self):
-        return self._dfs
-
-    @dfs.setter
-    def dfs(self, values):
-        #TODO protect this against err
-        xs, ys = values
-        self._xs = TSCConnection(xs, self._start_year)
-        self._ys = TSCConnection(ys, self._start_year)
-
     def _init(self):
-        _xs_series = self._xs._df[self._xs._col]
-        _ys_series = self._ys._df[self._ys._col]
-        self._dfs = DataFrame({self._xs_str: _xs_series, self._ys_str: _ys_series})
-        self._ret_dfs = self._dfs[1:].pct_change()
-        self._check_equal_lengths()
-        self.__data = {'price': self._dfs, 'returns': self._ret_dfs}
+        self._ret_df =  self.df[1:].pct_change()
+        self._dfs =     {'price': self.df, 'returns': self._ret_df}
 
     def _check_equal_lengths(self):
         '''
         warning if lengths do not match, for calculations
         return None
         '''
-        if len(self._xs._df) != len(self._ys._df):
+        if len(self._xs) != len(self._ys):
             print('warning: len mismatch: xs:%s ys:%s' % 
-                  (len(self._xs._df), len(self._ys._df)))
+                  (len(self._xs), len(self._ys)))
 
     def correlate(self):
         '''
@@ -173,9 +138,9 @@ class TSCPairs(object):
         return list of seaborn JointGrid objects
         '''
         figs = []
-        for name, _df in self.__data.items():
-            figs.append(jointplot(self._xs_str, 
-                                  self._ys_str, 
+        for name, _df in self._dfs.items():
+            figs.append(jointplot(self.tickers[0],
+                                  self.tickers[1], 
                                   _df, 
                                   kind='reg', 
                                   annot_kws={'title': name}))
@@ -187,11 +152,10 @@ class TSCPairs(object):
         (annualized) stdev of returns - volatility measure
         return plt
         '''
-        for name, _df in self.__data.items():
+        for name, _df in self._dfs.items():
             _df.dropna() \
                 .rolling(250) \
                 .std() \
                 .plot(title='250 window stdev - %s' % name)
         plt.show()
         return plt
-
