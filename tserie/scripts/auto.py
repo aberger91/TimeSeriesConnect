f0f3db17785369b1a4e9861bf05be9a9f7465638
Andrew Berger <Andrew.Berger@intlfcstone.com>
2017-04-12 14:35:50 -0500

    added autoregressions/correlations

--- tserie/scripts/auto.py
+++ tserie/scripts/auto.py
@@ -1,4 +1,4 @@
-from tserie import Remote
+from tserie import AutoRegressions
 import argparse
 
 def main():
@@ -6,12 +6,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('instrument', nargs=1)
     parser.add_argument('--start', '-s', type=str, required=True)
+    parser.add_argument('--end', '-e', type=str, required=True)
     parser.add_argument('--lag', '-l', type=str, required=False)
     args = parser.parse_args()
-    rem = Remote(*args.instrument, int(args.start))
-    mse = rem.autoregress()
-    plt.show()
-    print('Mean Squared Error: %s' % mse)
+    auto = AutoRegressions(*args.instrument, args.start, args.end)
+    auto.autocorr()
+    result = auto.autoregress()
+    #plt.show()
+    #print('Mean Squared Error: %s' % mse)
 
 if __name__ == '__main__':
     main()
--- tserie/tsc.py
+++ tserie/tsc.py
@@ -7,12 +7,14 @@ __author__ = 'Andrew Berger'
 import matplotlib.pyplot as plt
 from seaborn import jointplot, pairplot, plt
 from pandas.tools.plotting import autocorrelation_plot
+from statsmodels.tsa.stattools import adfuller, acf, pacf
 from sklearn.metrics import mean_squared_error
-from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARIMA
 from quandl import get as quandl_get
-from pandas import DataFrame
+import pandas as pd
 import datetime as dt
 from .config import *
+import numpy as np
 
 try:    from pandas_datareader import data as pdr
 except: raise TSCError('try: pip install pandas_datareader')
@@ -26,13 +28,13 @@ class ApiStream(object):
         self.product =  product
         self._start =   start
         self._end =     end
-        self._frame =   DataFrame
+        self._frame =   pd.DataFrame
         self._exists =  self._set_frame()
         self._series =  self._extract_series()
         
     def _set_frame(self):
         '''
-        stream data into pandas.DataFrame from online
+        stream data into pandas.pd.DataFrame from online
         return df
         '''
         if self.product in QPRODUCT:
@@ -188,7 +190,7 @@ class TimeSeriesBatch(object):
             _frame = _frame.ix[:self._min_len]
 
     def pairplot(self):
-        pairplot(DataFrame(self._frames), diag_kind="kde")
+        pairplot(pd.DataFrame(self._frames), diag_kind="kde")
         plt.show()
 
 
@@ -212,7 +214,7 @@ class TimeSeriesPairs(TimeSeriesBatch):
         figs = []
         for name, _frame in self._frames.items():
             xs, ys = self.product
-            dat = DataFrame({xs: self._frames[name]._series,
+            dat = pd.DataFrame({xs: self._frames[name]._series,
                              ys: self._frames[name]._ret_df})
             figs.append(jointplot(xs,
                                   ys,
@@ -237,23 +239,101 @@ class TimeSeriesPairs(TimeSeriesBatch):
         return plt
 
 
-class AutoRegressions(TimeSeriesFrame):
-    def autocorr(self):
-        autocorrelation_plot(self._series)
-        plt.show()
+class Stationary(TimeSeriesFrame):
+    def __init__(self, product, start, end, window=14):
+        super().__init__(product, start, end)
+        self.X = self._series.apply(np.log)
+        self.exp_avg = self.X.ewm(span=window).mean()
 
-    def autoregress(self, lag=14):
-        X = self._series.values
-        train, test = X[1:len(X)-lag], X[len(X)-lag:]
-        model = AR(train)
-        model_fit = model.fit()
-        predictions = model_fit.predict(start=len(train),
-                                        end=len(train)+len(test)-1,
-                                        dynamic=False)
-        error = mean_squared_error(test, predictions)
+    def plot_log(self):
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        ax.plot(test)
-        ax.plot(predictions)
+        ax.plot(self.X.index, self.X.values)        
+        ax.plot(self.X.index, self.exp_avg, color="red")
+
+    def dickyfuller(self, _series, window=14):
+        avg = _series.rolling(window=window).mean()  
+        std = _series.rolling(window=window).std()
+        adtest = adfuller(_series, lag='AIC')
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(_series.index, _series.values)
+        ax.plot(_series.index, avg, color='red')
+        ax.plot(_series.index, std, color='black')
+        dfoutput = pd.Series(dftest[0:4], 
+                             index=['Test Statistic',
+                                    'p-value',
+                                    '#Lags Used',
+                                    'Number of Observations Used'])
+        for key, value in dftest[4].items():
+            dfoutput['Critical Value (%s)' % key] = value
+        return adtest
+
+    def make_stationary_ewma(self, window=14, halflife=12):
+        diff = self.X - self.exp_avg
+        diff.dropna(inplace=True)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(diff.index, diff.values)
+        return diff
+
+    def make_stationary_shift(self):
+        diff = self.X - self.X.shift()
+        diff.dropna(inplace=True)
+        return diff
+
+
+class AutoRegressions(Stationary):
+    def autocorr(self, nlags=20):
+#        autocorrelation_plot(self._series)
+#        plt.show()
+        diff = self.make_stationary_shift()
+        lag_acf = acf(diff, nlags=nlags)
+        lag_pacf = pacf(diff, nlags=nlags, method='ols')
+        ''' Plot ACF '''
+        plt.subplot(121)
+        plt.plot(lag_acf)
+        plt.axhline(y=0,linestyle='--',color='gray')
+        plt.axhline(y=-1.96/np.sqrt(len(diff)),linestyle='--',color='gray')
+        plt.axhline(y=1.96/np.sqrt(len(diff)),linestyle='--',color='gray')
+        plt.title('Autocorrelation Function')
+        ''' Plot PACF '''
+        plt.subplot(122)
+        plt.plot(lag_pacf)
+        plt.axhline(y=0,linestyle='--',color='gray')
+        plt.axhline(y=-1.96/np.sqrt(len(diff)),linestyle='--',color='gray')
+        plt.axhline(y=1.96/np.sqrt(len(diff)),linestyle='--',color='gray')
+        plt.title('Partial Autocorrelation Function')
+        plt.tight_layout()
         plt.show()
-        return error
+
+    def autoregress(self, lag=14, order=(2, 1, 0)):
+        '''
+        order = (p, d, q)
+        '''
+       #train, test = self.X[1:len(self.X)-lag], self.X[len(self.X)-lag:]
+       #predictions = model_fit.predict(start=len(train),
+       #                                end=len(train)+len(test)-1,
+       #                                dynamic=False)
+       #error = mean_squared_error(test, predictions)
+        ''' Plot Stationary '''
+        diff = self.make_stationary_shift()
+        model = ARIMA(self.X, order=order)
+        model_fit = model.fit(disp=-1)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(diff)
+        ax.plot(model_fit.fittedvalues, color='red')
+        plt.title('RSS: %.4f' % sum((model_fit.fittedvalues - diff)**2))
+        plt.show()
+        ''' Plot to Scale '''
+        predict = pd.Series(model_fit.fittedvalues, copy=True)
+        predict_log = pd.Series(self.X.ix[0], index=self.X.index)
+        predict_log = predict_log.add(predict.cumsum(), fill_value=0)
+        predict_ARIMA = predict_log.apply(np.exp)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(self._series.index, self._series)
+        ax.plot(predict_ARIMA.index, predict_ARIMA.values)
+        plt.show()
+        return predict_ARIMA
